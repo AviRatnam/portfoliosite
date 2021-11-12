b481f5b6526f2239d679a1fe0ac08cddd49c6b87
Avinash Ratnam <54929315+AviRatnam@users.noreply.github.com>
2021-11-12 22:59:11 +0530

    Added new homepage features

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-        blue:{
-            charcoalblue: '#2f4858',
-            queenblue: '#33658A',
-            darkskyblue:'#86bbd8',
+    extend: {
+      colors: {
+        blue: {
+          charcoalblue: "#2f4858",
+          queenblue: "#33658A",
+          darkskyblue: "#86bbd8",
         },
-        yellow:{
-          brightyellowcrayola:'#F6AA1C'
-        }
+        yellow: {
+          brightyellowcrayola: "#F6AA1C",
+        },
+      },
     },
   },
   variants: {
